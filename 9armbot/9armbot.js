24ece7e4b7ca0a84c55e8ab28c0f9076917493b3
Thananon Patinyasakdikul <hualimz@gmail.com>
2021-02-10 08:22:28 +0700

    added coin implementations. Still exist only in memory but functionality is ok'ish.

--- 9armbot/9armbot.js
+++ 9armbot/9armbot.js
@@ -1,5 +1,6 @@
 const tmi = require('tmi.js');
 const fs = require('fs');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 var oauth_token = fs.readFileSync('oauth_token', 'utf8');
 
@@ -7,11 +8,108 @@ var critRate = 5;
 var critMultiplier = 1.5;
 var dodgeRate = 3;
 var baseTimeoutSeconds = 600;
+var botExp = 0;
+var botLevel = 1;
 
 var purgeMode = 0;
 var purgeList = [];
 
+var sentryMode = 1;
 var vengeanceMode=0;
+
+var coins = {};
+
+function deductCoins(user, amount) {
+    if (coins[user] == undefined) {
+        return false;
+    }
+
+    if (coins[user] < amount)
+        return false;
+
+    coins[user] -= amount;
+    return true;
+}
+
+
+function giveCoins(users, amount) {
+
+    let len = users.length;
+    for (i=0;i<len;i++) {
+        if (coins[users[i]] == undefined)
+            coins[users[i]] = amount;
+        else
+            coins[users[i]] += amount;
+        console.log(`${users[i]} has ${coins[users[i]]} coins.`);
+    }
+    console.log(`Gave out ${amount} coins to ${len} users.`);
+    return len;
+}
+
+function feedBot(channel, user, amount) {
+    if (deductCoins(user.username, amount)) {
+        botExp += amount;
+        if (botExp >= 500) { // level up
+            botExp -= 500;
+            baseTimeoutSeconds+=10;
+            botLevel++;
+            client.say(channel, `LEVEL UP!! ->${botLevel}`);
+        }
+    }
+}
+
+
+// return JSON: list of viewer's username.
+function getOnlineUsers (channel) {
+    //TODO: channel is currently hard-coded.
+    let query_url = `https://tmi.twitch.tv/group/user/armzi/chatters`;
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", query_url, false); //synchronous
+    xmlhttp.send();
+
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        chatterJSON = JSON.parse(xmlhttp.responseText);
+        return (chatterJSON['chatters']['viewers']);
+    }
+}
+
+function thanos (channel, byUser) {
+    let thanosCost = 30;
+    let thanosTimeoutSeconds = 300;
+    let casualties = 0;
+    if (deductCoins(byUser.username, thanosCost) || byUser == 'armzi') {
+        list = getOnlineUsers(channel);
+        for(i=0; i<list.length;i++) {
+            if (roll(50)){
+                casualties++;
+                // directly call timeout API as we dont want crit/dodge.
+                client.timeout(channel, list[i], thanosTimeoutSeconds, `โดนทานอสดีดนิ้ว`);
+            }
+        }
+        client.say(channel, `@${byUser.username} ใช้งาน Thanos Mode มี ${casualties} คนในแชทหายตัวไป....`);
+    } else {
+        timeoutUser(channel, byUser, baseTimeoutSeconds, `ค่าจ้างทานอส ${thanosCost} armcoin โว้ย..`);
+    }
+}
+
+function gacha(channel, user, amount) {
+    let gachaLegendaryRate = 5;
+    let gachaMysticRate = 15;
+    console.log(`gacha: ${channel}, ${user.username}, ${amount}`);
+    if (deductCoins(user.username, amount)) {
+        if (roll(gachaLegendaryRate)) {
+            coins[user.username] += amount*10;
+            client.say(channel, `@${user.username} ได้รางวัล ${amount*10} armcoin.`);
+        } else if (roll(gachaMysticRate)) {
+            coins[user.username] += amount*3;
+            client.say(channel, `@${user.username} ได้รางวัล ${amount*3} armcoin.`);
+        } else {
+            client.say(channel, `🧂🧂🧂 @${user.username} 🧂 LUL🧂🧂🧂🧂`);
+        }
+    } else {
+        timeoutUser(client.getChannel, user, baseTimeoutSeconds, `เล่นพนันไม่มีตังจ่าย ติดคุก`);
+    }
+}
 
 function timeoutUser(channel, user, duration, reason) {
 
@@ -75,17 +173,26 @@ client.connect();
 client.on('message', (channel, tags, message, self) => {
     if (self) return;
 
-    if (/[2๒].*[5๕].*[2๒].*[5๕]/.test(message)) {
-        timeoutUser(channel, tags, baseTimeoutSeconds, 'เก่งคณิตศาสตร์');
+    if (sentryMode) {
+        if (/[2๒]\s*[5๕]\s*([*xX]|คูณ|multiply)\s*[2๒]\s*[5๕]/i.test(message)) {
+            client.say(channel, '225 ไง Land Protector อะ');
+            if (roll (15))
+                timeoutUser(channel, tags, baseTimeoutSeconds, 'เก่งคณิตศาสตร์');
+            return;
+        }
+
+        let wanttofly = /อยากบิน.*/;
+        if (wanttofly.test(message)) {
+            if (roll(50))
+                timeoutUser(channel, tags, baseTimeoutSeconds, 'อยากบิน');
+            return;
+        }
     }
 
-    let wanttofly = /อยากบิน.*/;
-    if (wanttofly.test(message)) {
-        timeoutUser(channel, tags, baseTimeoutSeconds, 'อยากบิน');
+    if (message == '!botstat') {
+        client.say(channel, `<Level ${botLevel}> <EXP ${botExp}/500> <พลังโจมตี: ${baseTimeoutSeconds}> <%crit: ${critRate}> <ตัวคูณ: ${critMultiplier}>`);
+        return;
     }
-
-    if (message == '!botstat')
-        client.say(channel, `<พลังโจมตี: ${baseTimeoutSeconds}> <%crit: ${critRate}> <ตัวคูณ: ${critMultiplier}>`);
 
     // Hard coded command for me. We will have to handle priviledge later.
     if (message == '!reset' && tags.username == 'armzi') {
@@ -93,6 +200,7 @@ client.on('message', (channel, tags, message, self) => {
         critMultiplier = 2;
         baseTimeoutSeconds = 600;
         client.say(channel, '(RESET) โอกาศติดคริ = '+critRate+'% ตัวคูณ = '+critMultiplier+'.');
+        return;
     }
 
     if (message == '!purgemode' && tags.username == 'armzi') {
@@ -116,7 +224,58 @@ client.on('message', (channel, tags, message, self) => {
             client.say(channel, `☠️☠️' ชำระล้างเสร็จสิ้น ตัวคูณเพิ่มขึ้น ${critUp} จากการชำระล้าง`);
             critMultiplier += critUp;
         }
+        return;
     }
+
+    if (tags.username == "armzi" && message == '!sentry') {
+        if (sentryMode == 1) sentryMode = 0;
+        else sentryMode = 1;
+        return
+    }
+
+    if (message == '!give' && tags.username == "armzi") {
+        //giveCoins(getOnlineUsers(channel), 1);
+        client.say(channel, `สมาชิก ${giveCoins(getOnlineUsers(channel), 1)} รายได้รับ 1 armcoin`);
+        return;
+    }
+
+    if (message == '!coin') {
+        if (coins[tags.username])
+            client.say(channel, `@${tags.username} มี ${coins[tags.username]} armcoin.`);
+        else
+            client.say(channel, `@${tags.username} มี 0 armcoin.`);
+        return;
+    }
+
+    if (message == '!thanos') {
+        thanos(channel, tags);
+        return;
+    }
+
+    if (message == '!c') {
+        coins['armzi'] = 999;
+    }
+
+    /* Gacha XXXX */
+    let gacha_re = /^!gacha\s*(\d*)/;
+    let group = message.match(gacha_re);
+    if (group) {
+        if (!group[1])
+            gacha(channel, tags, 1);
+        else
+            gacha(channel, tags, parseInt(group[1]));
+        return;
+    }
+
+    let feed_re = /^!feed\s*(\d*)/;
+    group = message.match(feed_re);
+    if (group) {
+        if (!group[1])
+            feedBot(channel, tags, 1);
+        else
+            feedBot(channel, tags, parseInt(group[1]));
+    }
+
 
     if (message == '!vengeance' && tags.username == 'armzi') {
         if (vengeanceMode == 0) {
@@ -126,17 +285,21 @@ client.on('message', (channel, tags, message, self) => {
             vengeanceMode = 0;
             client.say(channel, `ไม่แค้นแล้วจ้า..`);
         }
+        return;
     }
 });
 
 client.on('subscription', (channel, username, method, message, userstate) => {
     critRate+=2;
     client.say(channel, `>> critRate+2% ด้วยพลังแห่งทุนนิยม (${critRate}%) <<`);
+    client.say(channel, `สมาชิก ${giveCoins(getOnlineUsers(channel), 1)} รายได้รับ 1 armcoin`);
+    giveCoins(getOnlineUsers(channel), 1);
 });
 
 client.on('resub', (channel, username, months, message, userstate, method) => {
     critRate+=2;
     client.say(channel, `>> critRate+2% ด้วยพลังแห่งทุนนิยม (${critRate}%) <<`);
+    client.say(channel, `สมาชิก ${giveCoins(getOnlineUsers(channel), 1)} รายได้รับ 1 armcoin`);
 });
 
 client.on('cheer', (channel, userstate, message) => {
