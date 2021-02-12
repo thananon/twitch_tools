2fd664e978ecf4d975353e63b97cd94ed568be65
jsirichai <jamesirichai.chulee@gmail.com>
2021-02-12 12:51:42 +0700

    [Refactor] async & await  syncPlayers :tada:

--- 9armbot/player.js
+++ 9armbot/player.js
@@ -36,10 +36,10 @@ class Player{
         fs.writeFileSync(this.options.database_path, data, 'utf8');
     }
 
-    syncPlayers(){
-        axios.get(`${process.env.twitch_api}/group/user/${this.options.channel}/chatters`).then((res)=>{
-            let data = res.data
-            // console.log(res)
+    async syncPlayers(){
+        try {
+         const {data} = await axios.get(`${process.env.twitch_api}/group/user/${this.options.channel}/chatters`);
+            // console.log(data)
             // let chatter_count = data.chatter_count
             let chatters = data.chatters
             for(let username of chatters.viewers){
@@ -56,14 +56,14 @@ class Player{
             // save
             this.saveData()
 
-        }).catch((err)=>{
+        } catch (err) {
             console.log('[syncPlayers] Error', err)
-        })
+        }
     }
 
     async getOnlinePlayers(){
-        let { data } = await axios.get(`${process.env.twitch_api}/group/user/${this.options.channel}/chatters`)
-        return data.chatters.viewers;
+            let { data } = await axios.get(`${process.env.twitch_api}/group/user/${this.options.channel}/chatters`)
+            return data.chatters.viewers;
     }
 
     create(username, level = 1, coins = 0, status = "offline"){
