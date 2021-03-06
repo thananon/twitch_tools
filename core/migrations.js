38888a6b16ba78cdcbae87060d7844a2aed0c2dd
Moki <kittisakphiri@gmail.com>
2021-03-06 17:15:04 +0700

    minify migration

--- core/migrations.js
+++ core/migrations.js
@@ -3,29 +3,14 @@ const playerMigrations = [
         from: "1.0",
         to: "1.1",
         up: schema => {
-            const newSchema = {
-                version: "1.1",
-                username: schema.username,
-                level: schema.level,
-                coins: schema.coins,
-                status: schema.status,
-                exp: schema.exp,
-                rollCounter: 0,
-                role: schema.role
-            };
-            return newSchema;
+            schema.version = "1.1";
+            schema.rollCounter = 0;
+            return schema;
         },
         down: schema => {
-            const newSchema = {
-                version: "1.0",
-                username: schema.username,
-                level: schema.level,
-                coins: schema.coins,
-                status: schema.status,
-                exp: schema.exp,
-                role: schema.role
-            };
-            return newSchema;
+            schema.version = "1.0";
+            delete schema.rollCounter;
+            return schema;
         }
     },
 ];
