8c7488a1460f9a1dfd8042e0f1a7aedba1adeb64
Moki <kittisakphiri@gmail.com>
2021-03-06 09:34:07 +0700

    fix wrong test

--- __test__/general.test.js
+++ __test__/general.test.js
@@ -9,7 +9,16 @@ test('sleep test', async () => {
 });
 
 test('roll guarantee test', () => {
-    const player = JSON.parse(JSON.stringify(newSchema));
+    const player = {
+        version: "1.1",
+        username: "TestUser",
+        level: 1,
+        coins: 0,
+        status: "online",
+        exp: 0,
+        rollCounter: 0,
+        role: "viewer"
+    };
     player.rollCounter = 99;
     const result = roll(1, player);
 
--- __test__/migrate.test.js
+++ __test__/migrate.test.js
@@ -26,7 +26,7 @@ test('player migration test.', () => {
     expect(oldSchema).toEqual(newSchema);
 });
 
-test('player migration test with exists database.', () => {
+test('player migration with exists database test.', () => {
     const oldSchema = {
         username: "TestUser",
         level: 1,
@@ -43,4 +43,26 @@ test('player migration test with exists database.', () => {
     fs.unlinkSync('migrate.json');
 
     expect(players[0]).toEqual(newSchema);
+});
+
+test('player migration exists database write to file test.', () => {
+    const oldSchema = {
+        username: "TestUser",
+        level: 1,
+        coins: 0,
+        status: "online",
+        exp: 0,
+        role: "viewer"
+    };
+    let players = [oldSchema];
+    let data = JSON.stringify(players);
+    fs.writeFileSync('migrate.json', data, 'utf8');
+    players = [newSchema];
+    data = JSON.stringify(players);
+    fs.writeFileSync('migrate.json', data, 'utf8');
+    let load = fs.readFileSync('migrate.json', 'utf8');
+    players = JSON.parse(load);
+    fs.unlinkSync('migrate.json');
+
+    expect(players.length).toBe(1);
 });
