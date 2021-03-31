9e74774de27a3e0a41eb6d4a2097a649b7547112
Guitared <guitared@users.noreply.github.com>
2021-03-31 21:07:12 +0700

    fix rollcounter not reset after successful roll

--- core/utils.js
+++ core/utils.js
@@ -9,12 +9,13 @@ const roll = (critRate, _player = null) => {
     dice = Math.random() * 100;
     if(_player && _player.rollCounter >= 0) {
         _player.rollCounter++;
-        if(_player.rollCounter >= 80) {
-            let rateUp =  (_player.rollCounter == 100)? 100 : _player.rollCounter / 10;
-            if(dice < critRate + rateUp) {
-                _player.rollCounter = 0;
-                return true;
-            }
+        let rateUp = 0;
+        if (_player.rollCounter >= 80) {
+            rateUp = (_player.rollCounter == 100)? 100 : _player.rollCounter / 10;
+        }
+        if (dice < critRate + rateUp) {
+            _player.rollCounter = 0;
+            return true;
         }
     }
     return dice < critRate;
