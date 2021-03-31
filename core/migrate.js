5317a23ffe8f31cc36c3a5412d5381a0e358609f
Guitared <guitared@users.noreply.github.com>
2021-03-31 20:38:33 +0700

    enable dynamic subversion for upOrDown function
    e.g. 1.0 vs 1.0.0.0.0.0.1

--- core/migrate.js
+++ core/migrate.js
@@ -1,12 +1,12 @@
 const upOrDown = (fromVersion, toVersion) => {
   const fromNumbers = fromVersion.split(".").map(el => Number(el));
   const toNumbers = toVersion.split(".").map(el => Number(el));
-  for (let i = 0; i < fromNumbers.length; i++) {
-    if (fromNumbers[i] < toNumbers[i]) {
-      return "up";
-    }
-    if (fromNumbers[i] > toNumbers[i]) {
-      return "down";
+  const longestLength = Math.max(fromNumbers.length, toNumbers.length);
+  for (let i = 0; i < longestLength; i++) {
+    if (!fromNumbers[i]) fromNumbers[i] = 0;
+    if (!toNumbers[i]) toNumbers[i] = 0;
+    if (fromNumbers[i] != toNumbers[i]) {
+      return (fromNumbers[i] < toNumbers[i]) ? "up" : "down";
     }
   }
   return "same";
@@ -22,7 +22,7 @@ const migrate = (schema, migrations, toVersion) => {
     return schema;
   }
   const currentMigration = migrations.find(
-    migration => migration[direction === "up" ? "from" : "to"] === fromVersion
+    migration => migration[direction === "up" ? "from" : "to"] === fromVersion;
   );
   const newSchema = currentMigration[direction](schema);
   return migrate(newSchema, migrations, toVersion);
