f80a93b685d0c07aeee89bb084102bd7bb5ebecc
Manassarn Manoonchai <manassarn@gmail.com>
2021-04-03 01:27:18 +0700

    feat: replace json db with prisma client

--- 9armbot-2.0/services/db.ts
+++ 9armbot-2.0/services/db.ts
@@ -1,88 +1,52 @@
-import find from 'lodash/find'
-import fs from 'fs'
-import { nanoid } from 'nanoid'
-import path from 'path'
-
-const DB_PATH = path.resolve(process.cwd(), './players-2.0.json')
+import { Player } from '@prisma/client'
+import prisma from '../../prisma/client'
 
 export interface IDb {
-  players: IPlayer[]
+  players: Player[]
 }
 
 export interface IPlayer {
-  uid: string
+  id: Number
   username: string
 }
 
 export class Db {
-  private db: IDb = {
-    players: [],
+  public async connect(): Promise<void> {
+    await prisma.$connect()
+    console.log('Database disconnected.')
   }
 
-  public load(): void {
-    try {
-      const playersJson = fs.readFileSync(DB_PATH, 'utf8')
-      this.db = JSON.parse(playersJson)
-      console.log(`Loaded ${this.db.players.length} players from ${DB_PATH}.`)
-    } catch (err) {
-      console.log('[ERROR] File not found, use default blank db.', err.message)
+  public async disconnect(): Promise<void> {
+    await prisma.$disconnect()
+    console.log('Database disconnected.')
+  }
+
+  public async read() {
+    return {
+      players: await prisma.player.findMany(),
     }
   }
 
-  public save(): void {
-    try {
-      const data = JSON.stringify(this.read(), null, 2)
-      fs.writeFileSync(DB_PATH, data, 'utf8')
-      console.log(`Saved ${this.db.players.length} players to ${DB_PATH}.`)
-    } catch (err) {
-      console.log('[ERROR] Cannot write file.', err.message)
-    }
-  }
-
-  public read(): IDb {
-    return this.db
-  }
-
-  public createPlayer(username: string): IPlayer {
+  public async createPlayer(username: string): Promise<Player> {
     // Don't re-create existing player
-    const player = this.getPlayerbyUsername(username)
+    const player = await this.getPlayerbyUsername(username)
 
     if (player) {
       return player
     }
 
-    const newPlayer: IPlayer = {
-      uid: nanoid(),
-      username,
-    }
-
-    this.db.players.push(newPlayer)
+    const newPlayer = await prisma.player.create({
+      data: {
+        username,
+      },
+    })
 
     return newPlayer
   }
 
-  public getPlayerbyUsername(username: string): IPlayer | undefined {
-    return find(this.db.players, (p) => {
-      return p.username === username
+  public async getPlayerbyUsername(username: string) {
+    return await prisma.player.findFirst({
+      where: { username },
     })
   }
 }
-
-// Graceful Shutdown
-function gracefulShutdown(): void {
-  console.log('\nPre-Close')
-  dbService.save()
-  setTimeout(() => {
-    console.log('Closed')
-    process.exit(0)
-  }, 1000)
-}
-process.on('SIGINT', () => gracefulShutdown())
-process.on('SIGTERM', () => gracefulShutdown())
-process.on('uncaughtException', (err) => {
-  console.error(err)
-  gracefulShutdown()
-})
-
-// Use singleton pattern to mitigate multiple readers/writers
-export const dbService = new Db()
--- 9armbot-2.0/test/db.test.ts
+++ 9armbot-2.0/test/db.test.ts
@@ -1,113 +1,66 @@
-import fs from 'fs'
-import { Db, IDb } from '../services/db'
-
-jest.mock('fs')
-
-const mockFs = fs as jest.Mocked<typeof fs>
+import prisma from '../../prisma/client'
+import { Db } from '../services/db'
 
 let db: Db
 
-beforeEach(() => {
+beforeEach(async () => {
   db = new Db()
+
+  // Cleanup players each test
+  await prisma.player.deleteMany()
 })
 
-describe('Database', () => {
-  describe('#load', () => {
-    it('loads data from json file and initiate players', () => {
-      const dbFileContent: IDb = {
-        players: [
-          {
-            uid: 'uid',
-            username: 'foo',
-          },
-        ],
-      }
-
-      mockFs.readFileSync.mockReturnValueOnce(JSON.stringify(dbFileContent))
-
-      db.load()
-
-      expect(db.read()).toEqual(dbFileContent)
-      expect(mockFs.readFileSync).toHaveBeenCalledTimes(1)
-    })
-
-    it('falls back to default if file does not exist', () => {
-      const dbFileBlank: IDb = {
-        players: [],
-      }
-
-      mockFs.readFileSync.mockImplementation(() => {
-        throw new Error('ENOENT: no such file or directory')
-      })
-
-      expect(db.read()).toEqual(dbFileBlank)
-      expect(mockFs.readFileSync).toHaveBeenCalledTimes(1)
-    })
-  })
-
-  describe('#save', () => {
-    it('saves the current database state to json file', () => {
-      db.createPlayer('foo')
-
-      const expectedJson = JSON.stringify(db.read(), null, 2)
-
-      db.save()
-
-      expect(mockFs.writeFileSync).toHaveBeenCalledTimes(1)
-      expect(mockFs.writeFileSync).toHaveBeenCalledWith(
-        expect.any(String),
-        expectedJson,
-        'utf8',
-      )
-    })
-  })
+afterAll(async () => {
+  await prisma.$disconnect()
 })
 
 describe('CRUD players', () => {
   describe('#createPlayer', () => {
-    it('can create new player by username (twitch for now)', () => {
-      db.createPlayer('foo')
+    it('can create new player by username (twitch for now)', async () => {
+      await db.createPlayer('foo')
 
-      expect(db.read()).toEqual({
+      expect(await db.read()).toEqual({
         players: [
-          {
-            uid: expect.any(String),
+          expect.objectContaining({
+            id: expect.any(Number),
             username: 'foo',
-          },
+          }),
         ],
       })
     })
 
-    it('does not create new player if username is existed', () => {
-      db.createPlayer('foo')
-      db.createPlayer('foo')
-      db.createPlayer('foo')
+    it('does not create new player if username is existed', async () => {
+      await db.createPlayer('foo')
+      await db.createPlayer('foo')
+      await db.createPlayer('foo')
 
-      expect(db.read()).toEqual({
+      expect(await db.read()).toEqual({
         players: [
-          {
-            uid: expect.any(String),
+          expect.objectContaining({
+            id: expect.any(Number),
             username: 'foo',
-          },
+          }),
         ],
       })
     })
   })
 
   describe('#getPlayerbyUsername', () => {
-    it('returns undefined if player is not found', () => {
-      expect(db.getPlayerbyUsername('foo')).toBeUndefined
+    it('returns undefined if player is not found', async () => {
+      expect(await db.getPlayerbyUsername('foo')).toBeUndefined
     })
 
-    it('returns player if found by username', () => {
-      db.createPlayer('foo')
+    it('returns player if found by username', async () => {
+      await db.createPlayer('foo')
 
-      expect(db.getPlayerbyUsername('foo')).toEqual({
-        uid: expect.any(String),
-        username: 'foo',
-      })
+      expect(await db.getPlayerbyUsername('foo')).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          username: 'foo',
+        }),
+      )
 
-      expect(db.getPlayerbyUsername('bar')).toBeUndefined
+      expect(await db.getPlayerbyUsername('bar')).toBeUndefined
     })
   })
 })
