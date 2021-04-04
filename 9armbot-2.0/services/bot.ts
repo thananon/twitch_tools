e0acd8fb81a4ea8aeee4a6a72719141c9608a7c2
Manassarn Manoonchai <manassarn@gmail.com>
2021-04-04 18:09:17 +0700

    feat: move coin logic to bot service

--- 9armbot-2.0/services/bot.ts
+++ 9armbot-2.0/services/bot.ts
@@ -1,1 +1,33 @@
-export class Bot {}
+import { Db } from './db'
+
+const db = new Db()
+
+interface Result {
+  error?: string
+  data?: number
+}
+
+interface ErrorResult extends Result {
+  error: string
+}
+
+interface CoinResult extends Result {
+  data: number
+}
+
+export const commands = {
+  coin: async (username?: string | null): Promise<CoinResult | ErrorResult> => {
+    if (!username) {
+      return { error: 'input_invalid' }
+    }
+
+    const player = await db.getPlayerbyUsername(username)
+    if (!player) {
+      return { error: 'player_not_found' }
+    }
+
+    return { data: player.coins }
+  },
+}
+
+export default commands
