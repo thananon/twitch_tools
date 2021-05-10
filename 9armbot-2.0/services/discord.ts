7de16db4304e67b70ca3a773916f24b87898b921
Manassarn Manoonchai <manassarn@gmail.com>
2021-05-10 21:42:07 +0700

    :sparkles: (2.0): Run webapp from 2.0, add !testwidget command

--- 9armbot-2.0/services/discord.ts
+++ 9armbot-2.0/services/discord.ts
@@ -1,5 +1,6 @@
 import Discord from 'discord.js'
 import commands, { isError } from './bot'
+import { testWidget } from './widget'
 
 const helpers = {
   buildEmbedMessage: (
@@ -70,6 +71,11 @@ export async function discordService() {
         break
       case '!command':
         console.log('TODO')
+        break
+      case '!testwidget':
+        if (process.env.NODE_ENV === 'development') {
+          await testWidget()
+        }
         break
       default:
         break
